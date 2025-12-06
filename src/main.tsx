1fc52953c341623564e48644b0bb6189853c4deb
Rostyslav Zakharchuk <rostislavzaharcuk@gmail.com>
2025-12-06 03:55:30 +0100

    Include modern-normalize in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
+import "modern-normalize";
+
 //import './index.css'
 import App from './components/App/App'
 
